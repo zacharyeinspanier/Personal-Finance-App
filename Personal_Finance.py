371c443e28758f03bf90a345ce78aa52e724b4fb
Zach Einspanier <zeinpanier@gmail.com>
2023-10-12 20:02:53 -0400

    Creating CLI

--- Personal_Finance.py
+++ Personal_Finance.py
@@ -1,9 +1,24 @@
 import sys
 import argparse
+import subprocess
 
 
 def main():
-    
+    """
+    Main runs the command line program
+
+    Basic Idea for our program
+    Account
+        createNewAccount
+        Change to accound
+        delete Account
+        print account
+    Statement
+        Add statement
+        Remove statement
+        Print statement and transactions
+
+    """
 
 
 if __name__ == "__main__":
